620bbe47426f48610ec063996e439e375a4b83e6
cllxjxjx <rizayevcelal1@gmail.com>
2026-04-18 16:17:52 +0400

    patient.h

--- patient.h
+++ patient.h
@@ -1,8 +1,17 @@
-//
-// Created by seyid on 4/14/2026.
-//
-
 #ifndef SAP_FINAL_PATIENT_H
 #define SAP_FINAL_PATIENT_H
-
+struct Patient {
+    int PatientID;
+    char name[20];
+    int age;
+    char gender[10];
+    char contact[100];
+    char medical_history[100];
+    struct Doctor* Doctor;
+    struct Patient* next;
+    struct Patient* prev;
+};
+void registerPatient();
+void updatePatient(int PatientID);
+void retrievePatient(int PatientID);
 #endif //SAP_FINAL_PATIENT_H
